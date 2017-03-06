e747ad6e12629e09783289ed55de0de09f141964
kongtoonarmy <kongtoonarmy@hotmail.com>
2017-03-06 09:02:47 +0700

    Try to use configureOptions() and mapped options

--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -5,6 +5,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use AppBundle\Entity\Task;
 
 class TaskType extends AbstractType
 {
@@ -12,8 +15,18 @@ class TaskType extends AbstractType
     {
         $builder
             ->add('task')
-            ->add('dueDate', DateType::class, array('widget' => 'single_text'))
+            ->add('dueDate', DateType::class, array(
+                'widget' => 'single_text',
+                'mapped' => false
+            ))
             ->add('save', SubmitType::class)
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Task::class,
+        ));
     }
 }
